68d83378a8cb6843db0b3c3850197bfe001818ce
sansh72 <153851665+sansh72@users.noreply.github.com>
2024-09-08 11:57:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS  # Import CORS
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import numpy as np
 import pandas as pd
 import joblib
-import time
 import os
 
 app = Flask(__name__)
@@ -13,38 +12,48 @@ CORS(app)
 classifier = joblib.load('model_filename.pkl')
 scaler = joblib.load('scaler_filename.pkl')
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get JSON data from the request
-    data = request.get_json()
-    str_data = str(data)
+    try:
+        # Get JSON data from the request
+        data = request.get_json()
+        str_data = str(data)
+        
+        with open('output.txt', 'w') as file:
+            file.write(str_data)
+        
+        # Convert JSON data to DataFrame
+        df = pd.DataFrame([data])  # Wrap in a list to create a DataFrame with one row
 
-    with open('output.txt', 'w') as file:
-        file.write(str_data)
-    # Convert JSON data to DataFrame
+        # Check if 'Outcome' column exists and drop it
+        if 'Outcome' in df.columns:
+            df = df.drop(columns='Outcome')
 
-    df = pd.DataFrame([data])  # Wrap in a list to create a DataFrame with one row
-    df = df.drop(columns='Outcome',axis = 1)
+        # Convert to numpy array and reshape
+        numpy_array = np.asarray(df)
+        numpy_reshaped = numpy_array.reshape(1, -1)
 
-    # Convert to numpy array and reshape
-    numpy_array = np.asarray(df)
-    numpy_reshaped = numpy_array.reshape(1, -1)
+        # Standardize the input data
+        std_data = scaler.transform(numpy_reshaped)
 
-    # Standardize the input data
-    scaler.fit(numpy_reshaped)
-    std_data = scaler.transform(numpy_reshaped)
+        # Make prediction
+        prediction = classifier.predict(std_data)
 
-    # Make prediction
-    prediction = classifier.predict(std_data)
+        # Return the result
+        if prediction[0] == 1:
+            result = "The person is diabetic"
+        else:
+            result = "The person isn't diabetic"
 
-    # Return the result
-    if prediction[0] == 1:
-        result = "The person is diabetic"
-    else:
-        result = "The person isn't diabetic"
-
-    return jsonify({'result': result})
-
+        return jsonify({'result': result})
+    
+    except Exception as e:
+        app.logger.error(f'Error in predict function: {e}')
+        return jsonify({'error': 'An error occurred during prediction'}), 500
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Use the port set by the environment, default to 5000
