94f86db9bef5e8e2153fcb0232faa961cdeda6c4
sansh72 <153851665+sansh72@users.noreply.github.com>
2024-09-15 11:52:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS  # Import CORS
 import numpy as np
 import pandas as pd
